b406288c67f00a04a6a64e422bef84391e3f6de4
Niño James Tan <njtan142@gmail.com>
2025-01-12 22:04:17 +0800

    changed preview

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, presetUno, presetWebFonts } from "unocss";
 
 export default defineConfig({
   content: {
-    filesystem: ["**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
+    filesystem: ["src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
   },
   theme: {
     boxShadow: {
